2214529f87d864fc6a567631b25dcb2722408c9a
hoo <bhwkd@naver.com>
2021-10-11 03:33:15 +0900

    Create MultiplicationTable.cpp

---
+++ MultiplicationTable.cpp
@@ -0,0 +1,16 @@
+//this file make MultiplicationTable
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	//1단 부터 8단 까지 출력
+	for (int i = 1; i < 9; i++) {
+		cout << "< " << i << "단 " << ">" << endl;
+		for (int j = 1; j < 10; j++) {
+			cout << i << " * " << j << " = " << i * j << endl;
+		}
+		cout << endl;
+	}
+	return 0;
+}
