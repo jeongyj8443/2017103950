1c8d7ee78d7f5daadfcd0ed1b98dc20fef8c37af
jeongyj <init.jeongyj8443@khu.ac.kr>
2021-10-11 13:35:06 +0900

    Modify 9단

--- MultiplicationTable.cpp
+++ MultiplicationTable.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 	//1단 부터 8단 까지 출력
-	for (int i = 1; i < 9; i++) {
+	for (int i = 1; i < 10; i++) {
 		cout << "< " << i << "단 " << ">" << endl;
 		for (int j = 1; j < 10; j++) {
 			cout << i << " * " << j << " = " << i * j << endl;
